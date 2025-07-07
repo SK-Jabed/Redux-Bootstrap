34d6b2fbcf855381a827e0e332658746bd5a7e18
Sheikh Jabed <jabedbd2295@gmail.com>
2025-07-07 22:57:09 +0600

    [ADDED]: New Activities Added on Main File

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import router from './routes/index.tsx'
-import { ThemeProvider } from './providers/theme-provider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import router from "./routes/index.tsx";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
